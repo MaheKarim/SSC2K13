da0c07dc1e6952522052c8eac5a06f2f01685d41
Mahe Karim <mahekarim@gmail.com>
2026-03-02 05:06:02 +0600

    :bento: Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Admin;
-use App\Models\PhoneNumber;
-use App\Models\JerseySize;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
@@ -17,47 +15,12 @@ class DatabaseSeeder extends Seeder
     {
         // Create default admin
         Admin::firstOrCreate(
-            ['email' => 'admin@ssc2013.com'],
+            ['email' => 'general@ssc2013.com'],
             [
-                'name' => 'Admin',
-                'password' => Hash::make('admin123'),
+                'name' => 'General Admin',
+                'password' => Hash::make('general@ssc2013'),
                 'is_active' => true,
             ]
         );
-
-        // Create another admin
-        Admin::firstOrCreate(
-            ['email' => 'sarif@ssc2013.com'],
-            [
-                'name' => 'Sarif',
-                'password' => Hash::make('@ssc2013'),
-                'is_active' => true,
-            ]
-        );
-
-        // Create phone numbers
-        // $phoneNumbers = [
-        //     ['number' => '01711111111', 'operator' => 'bKash'],
-        //     ['number' => '01811111111', 'operator' => 'Nagad'],
-        //     ['number' => '01911111111', 'operator' => 'Rocket'],
-        // ];
-
-        // foreach ($phoneNumbers as $phone) {
-        //     PhoneNumber::create([
-        //         'number' => $phone['number'],
-        //         'operator' => $phone['operator'],
-        //         'active' => true,
-        //     ]);
-        // }
-
-        // Create jersey sizes
-        // $sizes = ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'];
-
-        // foreach ($sizes as $size) {
-        //     JerseySize::create([
-        //         'size' => $size,
-        //         'active' => true,
-        //     ]);
-        // }
     }
 }
