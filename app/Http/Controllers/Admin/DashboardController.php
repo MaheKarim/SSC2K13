80cc81acb4a5e6b2346aa23faeb09037655ec078
Mahe Karim <mahekarim@gmail.com>
2026-03-22 01:57:58 +0600

    :rotating_light: Site Update

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -46,12 +46,12 @@ class DashboardController extends Controller
                 'sent_to_phone_id',
                 \Illuminate\Support\Facades\DB::raw('count(*) as count'),
                 \Illuminate\Support\Facades\DB::raw('sum(amount) as total_amount'),
-                \Illuminate\Support\Facades\DB::raw('sum(case when donation_type = "iftar" then 1 else 0 end) as iftar_count'),
-                \Illuminate\Support\Facades\DB::raw('sum(case when donation_type = "iftar" then amount else 0 end) as iftar_amount'),
-                \Illuminate\Support\Facades\DB::raw('sum(case when donation_type = "jersey" then 1 else 0 end) as jersey_count'),
-                \Illuminate\Support\Facades\DB::raw('sum(case when donation_type = "jersey" then amount else 0 end) as jersey_amount'),
-                \Illuminate\Support\Facades\DB::raw('sum(case when donation_type = "both" then 1 else 0 end) as both_count'),
-                \Illuminate\Support\Facades\DB::raw('sum(case when donation_type = "both" then amount else 0 end) as both_amount'),
+                \Illuminate\Support\Facades\DB::raw('sum(case when donation_type = "iftar" AND (type IS NULL OR type != "sponsor") then 1 else 0 end) as iftar_count'),
+                \Illuminate\Support\Facades\DB::raw('sum(case when donation_type = "iftar" AND (type IS NULL OR type != "sponsor") then amount else 0 end) as iftar_amount'),
+                \Illuminate\Support\Facades\DB::raw('sum(case when donation_type = "jersey" AND (type IS NULL OR type != "sponsor") then 1 else 0 end) as jersey_count'),
+                \Illuminate\Support\Facades\DB::raw('sum(case when donation_type = "jersey" AND (type IS NULL OR type != "sponsor") then amount else 0 end) as jersey_amount'),
+                \Illuminate\Support\Facades\DB::raw('sum(case when donation_type = "both" AND (type IS NULL OR type != "sponsor") then 1 else 0 end) as both_count'),
+                \Illuminate\Support\Facades\DB::raw('sum(case when donation_type = "both" AND (type IS NULL OR type != "sponsor") then amount else 0 end) as both_amount'),
                 \Illuminate\Support\Facades\DB::raw('sum(case when type = "sponsor" then 1 else 0 end) as sponsor_count'),
                 \Illuminate\Support\Facades\DB::raw('sum(case when type = "sponsor" then amount else 0 end) as sponsor_amount')
             )
